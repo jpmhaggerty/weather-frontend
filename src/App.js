b32b84f2fc5b714bfd657d9f72ec6740f44a02de
jphaggerty <james.haggerty.1@spaceforce.mil>
2022-02-07 17:53:18 -0500

    Initial linkage to back-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import BasicCard from "./components/BasicCard";
+
+const axios = require("axios");
+
+const dataURI = "http://localhost:3001/api/";
 
 function App() {
+  const [dataFeed, setDataFeed] = useState([]);
+
+  useEffect(() => {
+    const getDataFeed = () => {
+      axios
+        .get(dataURI)
+        .then((response) => {
+          if (response.status >= 400) {
+            setDataFeed([]);
+            throw new Error("Bad response from server");
+          }
+          setDataFeed(
+            response.data.sort((first, second) => first.number - second.number)
+          );
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+
+    getDataFeed();
+  }, []);
+
+  let weatherForecast = dataFeed.map((element) => {
+    return <BasicCard key={element.number} dataFeed={element} />;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{weatherForecast}</header>
     </div>
   );
 }
---
+++ src/components/BasicCard.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+export default function BasicCard({ dataFeed }) {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {dataFeed?.name}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Temperature: {dataFeed?.temperature} °{dataFeed?.temperatureUnit} {dataFeed?.temperatureTrend}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Wind: {dataFeed?.windSpeed}, {dataFeed?.windDirection}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <img src={dataFeed?.icon} />
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {dataFeed?.shortForecast}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
