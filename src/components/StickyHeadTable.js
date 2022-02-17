888252a5b1eb882d2136a5dfdcc4ba3ab4229e5e
jphaggerty <jpm.haggerty@gmail.com>
2022-02-17 12:40:49 -0500

    Small update formatting

--- src/components/StickyHeadTable.js
+++ src/components/StickyHeadTable.js
@@ -52,7 +52,7 @@ export default function StickyHeadTable({ dataFeed }) {
 
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+      <TableContainer sx={{ maxHeight: 500 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
